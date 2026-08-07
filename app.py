e58d4990ff851ba5918c7d998a47f1e45a7790c1
Santiago-DS-ML <appiaelie06@gmail.com>
2026-08-07 22:53:16 +0000

    Changement de package

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_google_genai import ChatGoogleGenerativeAI
-from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_retrieval_chain
 from langchain_core.messages import HumanMessage, AIMessage
 
 st.set_page_config(page_title= 'Jango')
